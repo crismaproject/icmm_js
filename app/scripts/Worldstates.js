dcf354b037d57dd9e3afe32599d8cff8d21be402
Daniel Meiers <daniel.meiers@cismet.de>
2014-06-24 08:22:14 +0200

    closed #1

--- app/scripts/Worldstates.js
+++ app/scripts/Worldstates.js
@@ -208,15 +208,60 @@ angular.module(
                         augmentWorldstateDataWithRenderingDescriptors(worldstatesArr[i]);
                     }
                     return worldstatesArr;
-                };
-            var Worldstate = $resource(
-                CRISMA_ICMM_API + '/' + CRISMA_DOMAIN + '.worldstates/:wsId',
-                {wsId: '@id', deduplicate: false, level: '5', omitNullValues: 'false'},
-                {
+                },
+                Worldstate = $resource(
+                    CRISMA_ICMM_API + '/' + CRISMA_DOMAIN + '.worldstates/:wsId',
+                    {
+                    wsId: '@id',
+                    deduplicate: false,
+                    level: '5',
+                    omitNullValues: 'false'
+                },
+                    {
                     'get': {method: 'GET', transformResponse: processResult},
                     'query': {method: 'GET', isArray: true, transformResponse: processResults}
                 }
-            );
+                ),
+                worldstateUtils = (function () {
+                    var publicApi;
+
+                    publicApi = {};
+
+                    publicApi.stripIccData = function (worldstates, forCriteria) {
+                        var data, dataVector, i, iccdata, j, k, worldstate;
+
+                        dataVector = [];
+                        for (i = 0; i < worldstates.length; ++i) {
+                            worldstate = worldstates[i];
+                            iccdata = worldstate.iccdata;
+                            data = null;
+                            for (j = 0; j < iccdata.length && !data; ++j) {
+                                for (k = 0; k < iccdata[j].categories.length && !data; ++k) {
+                                    if (forCriteria && 'Criteria' === iccdata[j].categories[k].key) {
+                                        data = iccdata[j];
+                                    } else if (!forCriteria && 'Indicators' === iccdata[j].categories[k].key) {
+                                        data = iccdata[j];
+                                    }
+                                }
+                            }
+
+                            if (!data) {
+                                throw 'worldstate without proper icc data:' + worldstate;
+                            }
+
+                            dataVector.push({
+                                name: worldstate.name,
+                                data: JSON.parse(data.actualaccessinfo)
+                            });
+                        }
+
+                        return dataVector;
+                    };
+
+                    return publicApi;
+                })();
+
+            Worldstate.utils = worldstateUtils;
             return Worldstate;
         }
     ]
