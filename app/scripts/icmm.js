00be7bccba2aa84b7275b9bab5f89f7c0a44602a
Martin Scholl <martin.scholl@cismet.de>
2014-10-07 18:08:32 +0200

    [issue #7] icmm service with getNextId

---
+++ app/scripts/icmm.js
@@ -0,0 +1,58 @@
+angular.module(
+    'de.cismet.crisma.ICMM.services',
+    [
+        'ngResource'
+    ]
+).factory(
+    'de.cismet.crisma.ICMM.services.icmm',
+    [
+        '$q',
+        '$resource',
+        function ($q, $resource) {
+            'use strict';
+
+            var getNextId;
+
+            getNextId = function (apiurl, classkey) {
+                var def, Resource, objects;
+                def = $q.defer();
+                Resource = $resource(apiurl + classkey, {limit: '999999999'},
+                    {
+                        'query': {method: 'GET', isArray: true, transformResponse: function (data) {
+                            // we strip the ids of the objects only
+                            var col, res, i;
+
+                            col = JSON.parse(data).$collection;
+                            res = [];
+
+                            for (i = 0; i < col.length; ++i) {
+                                res.push(col[i]);
+                            }
+
+                            return res;
+                        }}
+                    });
+                objects = Resource.query();
+                objects.$promise.then(function (data) {
+                    var i, id, maxId;
+
+                    maxId = 0;
+
+                    for (i = 0; i < data.length; ++i) {
+                        id = parseInt(data[i].$ref.substr(data[i].$ref.lastIndexOf('/') + 1), 10);
+                        if (id > maxId) {
+                            maxId = id;
+                        }
+                    }
+                    def.resolve(maxId + 1);
+                });
+
+                return def.promise;
+            };
+
+            return {
+                getNextId: getNextId
+            };
+        }
+    ]
+);
